8ab70888fc95f1adb59bc33747932c0d214030f3
Akora sheenaz N <sheenkora60@gmail.com>
2026-09-09 13:09:46 +0300

    COMPLETE ASSIGNMENT

---
+++ AKORA SHEENAZ NAKASUJA/hello.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+
+int main() {
+    std::cout << "Welcome to C++\n";
+    std::cout << "Welcome to object oriented programming\n";
+    std::cout << "and programming is fun\n";
+    
+    return 0;
+}
